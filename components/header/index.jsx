a9fc581401a00cc67dba6d602ad6bbe810fe157f
chrislam365 <cllam@wpi.edu>
2024-12-21 18:52:40 -0500

    icons for this dark mode

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -6,9 +6,15 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { opacity, background } from './anim';
 import Nav from './nav';
+import {SunMedium, Moon} from "lucide-react";
 
 export default function Header() {
     const [isActive, setIsActive] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false); //toggle out light/dark mode
+
+    const toggleTheme = () => {
+        setIsDarkMode(!isDarkMode);
+    }
 
     return (
         <div className={styles.header}>
@@ -17,16 +23,24 @@ export default function Header() {
                     <Image src="/choso2.png" alt="logo" width={87} height={48}
                            className="rounded-lg  items-center"/>
                 </Link>
-                <div
-                    onClick={() => setIsActive(!isActive)}
-                    className={styles.el}
-                >
-                    <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
-                    <div className={styles.label}>
-                        <motion.p variants={opacity} animate={!isActive ? "open" : "closed"}></motion.p>
-                        <motion.p variants={opacity} animate={isActive ? "open" : "closed"}></motion.p>
+
+                <div className={"flex gap-x-6 items-center cursor-pointer"}>
+                    <div onClick={toggleTheme}>
+                        {isDarkMode ? (
+                            <Moon width={25} height={25}/>
+                        ) : (
+                            <SunMedium width={28} height={28}/>
+                        )}
+                    </div>
+                    <div onClick={() => setIsActive(!isActive)} className={styles.el}>
+                        <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+                        <div className={styles.label}>
+                            <motion.p variants={opacity} animate={!isActive ? "open" : "closed"}></motion.p>
+                            <motion.p variants={opacity} animate={isActive ? "open" : "closed"}></motion.p>
+                        </div>
                     </div>
                 </div>
+
             </div>
             <motion.div
                 variants={background}
@@ -35,7 +49,7 @@ export default function Header() {
                 className={styles.background}
             ></motion.div>
             <AnimatePresence mode="wait">
-                {isActive && <Nav setIsActive={setIsActive} />}
+                {isActive && <Nav setIsActive={setIsActive}/>}
             </AnimatePresence>
         </div>
     );
