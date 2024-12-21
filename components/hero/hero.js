d8a31f69398d673f3466a062654120b7eaf2a3f3
chrislam365 <cllam@wpi.edu>
2024-12-21 18:43:57 -0500

    hero section with animations completed

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,12 +1,22 @@
 "use client";
 import Link from "next/link";
 import { ArrowDown } from "lucide-react";
+import { motion } from "framer-motion";
 
 export default function Hero() {
+    const fadeIn = {
+        hidden: { opacity: 0 },
+        show: { opacity: 1, transition: { duration: 0.75 } },
+    };
+
     return (
         <section className="pt-14 min-h-[85vh] flex flex-col">
-            <div className="max-w-[85rem] flex-grow">
-                <h1 className="text-slight-black font-medium text-4xl md:text-6xl lg:text-7xl">
+            <div className="max-w-[100rem] flex-grow">
+                <motion.h1
+                    className={"text-slight-black font-medium text-4xl md:text-6xl lg:text-[5rem]"}
+                    initial={{ opacity: 0, x: 15 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ type: "spring", stiffness: 25, damping: 12 }}>
                     I&#39;m Chris Lam, a creative web developer driven by authentic expression.
                     I&#39;ll be interning at{" "}
                     <span className="text-spotify hover:text-slight-black">
@@ -16,10 +26,14 @@ export default function Hero() {
                     <span className="text-spotify hover:text-slight-black">
                         <Link href={"https://afciviliancareers.com/"}>AFCS</Link>
                     </span>.
-                </h1>
+                </motion.h1>
             </div>
 
-            <footer>
+            <motion.footer
+                initial="hidden"
+                animate="show"
+                variants={fadeIn}
+            >
                 <div className="flex gap-x-2">
                     <p className="border border-gray-300 rounded-full px-2 py-1 text-md inline-block text-slight-black">Front-End</p>
                     <p className="border border-gray-300 rounded-full px-4 py-1 text-md inline-block text-slight-black">UI/UX</p>
@@ -35,7 +49,7 @@ export default function Hero() {
                         </p>
                     </Link>
                 </div>
-            </footer>
+            </motion.footer>
         </section>
     );
 }
