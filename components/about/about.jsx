d54281ea22d7156c3308c847dbe61a07c4cafac0
chrislam365 <cllam@wpi.edu>
2024-12-25 22:29:03 -0500

    About section completely done

--- components/about/about.jsx
+++ components/about/about.jsx
@@ -1,13 +1,27 @@
+"use client"
 import Link from "next/link";
+import {ArrowDownToLine} from "lucide-react";
+import "@/app/globals.css"
+import { useRef } from "react";
+import {motion, useInView} from "framer-motion";
 
 export default function About() {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+
+
     return (
-        <section id={"#about"} className={"xl:px-48 2xl:px-60"}>
+        <section id={"#about"} className={"xl:px-48 2xl:px-60"}  ref={ref}>
             <div className={"grid grid-cols-1 lg:grid-cols-12 gap-x-8 pb-20"}>
                 <div className={"border-t border-t-[#e1e1e1] sidebar col-span-12 lg:col-span-5 xl:col-span-5"} id={"newLine"}>
                     <h1 className={"text-slight-black text-4xl md:text-5xl mt-4 font-normal xl:w-full"}>About</h1>
                 </div>
-                <div className={"mt-20 lg:mt-6 col-span-12 lg:col-span-7 xl:col-span-7 lg:max-w-[45rem]"}>
+                <motion.div
+                    className={"mt-20 lg:mt-6 col-span-12 lg:col-span-7 xl:col-span-7 lg:max-w-[45rem]"}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={isInView ? { opacity: 1, y: 0 } : {}}
+                    transition={{ duration: 0.5, delay: 0.2 }}
+                >
                     <h1 className={"text-2xl lg:text-3xl font-thin"}>
                         I&#39;m a creative web developer studying CS at Worcester Polytechnic Institute. In 2025, I will
                         be interning with
@@ -33,7 +47,11 @@ export default function About() {
                         <br />
                         In my downtime, I love to olympic lift, watch anime, and explore new cafes!
                     </h1>
-                </div>
+                    <a href="/resume.pdf" target="_blank" rel="opener referrer" className={"inline-flex mt-8  hover:bg-[#61E88A] transition   gap-x-1.5 bg-spotify px-4 py-3 rounded-full items-center"}>
+                        <ArrowDownToLine width={20} height={20} id={"antiDark"}/>
+                        <h1 className={"text-xl"} id={"antiDark"}>Download CV</h1>
+                    </a>
+                </motion.div>
             </div>
         </section>
     );
