abf4c8d8b2bf32083e19eb6f309ed1251821648e
chrislam365 <cllam@wpi.edu>
2024-12-26 17:03:57 -0500

    completed sections of landing pages. it is time for the case studies. btw i did new cursor and project animations

--- components/work/project1.jsx
+++ components/work/project1.jsx
@@ -1,25 +1,26 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import {ArrowRight} from "lucide-react";
+import { ArrowRight } from "lucide-react";
+import "@/app/globals.css"
 
-export default function project1() {
+export default function Project1() {
     return (
-        <div className={"group"}>
-            <Link href={"/kyoto-project-center"} className={"relative z-0 "}>
+        <div className="group">
+            <Link href="/kyoto-project-center" className="relative z-0">
                 <div>
-                    <Image src={"/mockup1.1.png"} alt={"KyotoMockup"} width={4000} height={2800} className={"w-full rounded-2xl drop-shadow-lg transition-transform duration-300 ease-in-out hover:scale-[102%] min-h-[240px] object-cover"}/>
-                    <div className={" right-1 absolute top-4 z-50 rounded-full p-4 bg-spotify hidden group-hover:inline-block transition duration-300 ease-in-out"}>
-                        <ArrowRight className={"text-black "} width={25} height={25}/>
+                    <Image src="/mockup1.1.png" alt="KyotoMockup" width={4000} height={2800} className="w-full rounded-2xl drop-shadow-lg transition-transform duration-300 ease-in-out hover:scale-[102%] min-h-[240px] object-cover"/>
+                    <div className="right-4 absolute top-4 z-50 rounded-full p-4 bg-spotify opacity-0 group-hover:opacity-100 translate-x-0 group-hover:translate-x-4 transition duration-300 ease-in-out" id={"arrowButton"}>
+                        <ArrowRight className="text-black" width={25} height={25} />
                     </div>
                 </div>
-                <div className={"mt-2"}>
+                <div className="mt-2">
                     <h1>Kyoto Project Center</h1>
-                    <h1 className={"text-slight-black"} id={"based"}>Explore WPI Kyoto IQPs</h1>
+                    <h1 className="text-newGray" id="based">
+                        Explore WPI Kyoto IQPs
+                    </h1>
                 </div>
             </Link>
-
         </div>
-
-    )
+    );
 }
--- components/work/project2.jsx
+++ components/work/project2.jsx
@@ -1,16 +1,25 @@
-"use client"
-
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-
-export default function project2() {
+import { ArrowRight } from "lucide-react";
+import "@/app/globals.css"
+export default function Project1() {
     return (
-        <Link href={"/iqp-dct"}>
-            <Image src={"/mockup2.png"} alt={"IQP"} width={4000} height={2800} className={"w-full rounded-2xl drop-shadow-lg transition-transform duration-300 ease-in-out hover:scale-[102%]"}/>
-            <div className={"mt-2"}>
-                <h1>IQP Data Collection Tool</h1>
-                <h1 className={"text-newGray"} id={"based"}>Major Qualifying Project</h1>
-            </div>
-        </Link>
-    )
+        <div className="group">
+            <Link href="/iqp-dct" className="relative z-0">
+                <div>
+                    <Image src="/mockup2.png" alt="KyotoMockup" width={4000} height={2800} className="w-full rounded-2xl drop-shadow-lg transition-transform duration-300 ease-in-out hover:scale-[102%] min-h-[240px] object-cover"/>
+                    <div className="right-4 absolute top-4 z-50 rounded-full p-4 bg-spotify opacity-0 group-hover:opacity-100 translate-x-0 group-hover:translate-x-4 transition duration-300 ease-in-out" id={"arrowButton"}>
+                        <ArrowRight className="text-black" width={25} height={25} />
+                    </div>
+                </div>
+                <div className="mt-2">
+                    <h1>IQP DCT</h1>
+                    <h1 className="text-newGray" id="based">
+                        Major Qualifying Project
+                    </h1>
+                </div>
+            </Link>
+        </div>
+    );
 }
