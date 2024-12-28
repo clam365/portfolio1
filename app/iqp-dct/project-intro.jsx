2b3c7f3f9fec3f15bf35f01bb405d39ac609ee3f
chrislam365 <cllam@wpi.edu>
2024-12-27 21:00:51 -0500

    IQP description changed

--- app/iqp-dct/project-intro.jsx
+++ app/iqp-dct/project-intro.jsx
@@ -6,8 +6,7 @@ export const ProjectIntro = () => {
         <div className={"grid grid-cols-1 md:grid-cols-2 mt-10  px-10  lg:px-48 xl:px-60 gap-x-10 gap-y-10"}>
             <div className={"border-t border-t-[#e1e1e1] py-2"} id={"newLine"}>
                 <h1 className={"text-2xl"}>
-                    Approximately 1,200 juniors each year participate in a 7 week humanitarian project across the globe known as the Interactive Qualifying Project.
-
+                    The IQP Data Collection Tool is tailored for WPI undergrads to collect and analyze their data across the globe, competing against current industry applications.
                     <br/>
                     <br/>
                     In this PERN stack app, I led the design and development of the responsive UI and conducted user research and testing.
