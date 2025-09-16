431b3435a31be2fdd71a713f132123a75e120845
chrislam365 <cllam@wpi.edu>
2025-09-16 16:51:03 -0400

    IQP DCT Content structured in newer components; needs updates on visuals

--- app/iqp-dct/project-background.jsx
+++ app/iqp-dct/project-background.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import Stats from "@/app/iqp-dct/stats";
-import ProjectGoal from "@/app/iqp-dct/project-goal";
-import Research from "@/app/iqp-dct/research";
-import TestingNumbers from "@/app/iqp-dct/testingNumbers";
 import Gallery from "@/app/iqp-dct/gallery";
+import {CaseStudySection} from "@/components/caseStudy/caseStudySection";
+import {List} from "@/components/List";
+import {AppWindow, ListOrdered, Sprout} from "lucide-react";
 
 export const ProjectBackground = () => {
     return (
         <div className={"px-10  xl:px-72 py-24 md:pt-48"}>
-            <div className={"border-t border-t-[#e1e1e1] my-16  py-2"} id={"newLine"}>
-                <div className={"grid grid-cols-1 md:grid-cols-2 gap-y-16"}>
-                    <div>
-                        <h1 className={"text-slight-black text-4xl font-medium xl:w-full"}>What&#39;s IQP DCT?</h1>
-                        <h1 className={"text-newGray text-3xl font-medium xl:w-full md:max-w-[80%]"} id={"based"}>A
-                            personalized data tool for WPI undergrads</h1>
-                    </div>
-                    <div>
+            <CaseStudySection>
+                <CaseStudySection.SubSection>
+                    <CaseStudySection.Title title={"What's IQP DCT?"} subtitle={"A personalized data tool for WPI undergrads"}/>
+                    <CaseStudySection.Content>
                         <h1 className={""}>
                             Worcester Polytechnic Institute juniors participate in a humanitarian project
                             known as the Interactive Qualifying Project (IQP) in any part of the globe. Our tool serves
@@ -23,20 +18,30 @@ export const ProjectBackground = () => {
                             application that helps students collect interviews and qualitative/quantitative data, along
                             with services such as
                             geotagging, transcriptions, and team management.
+                            <div className={"mt-10"}/>
+                            <List>
+                                <List.Header header={"Some numbers for context"}/>
+                                <List.Section>
+                                    <h1 className={"text-5xl font-medium"}>1,200</h1>
+                                    <h1 className={"my-1"}>juniors travel per year</h1>
+                                </List.Section>
+                                <List.Section>
+                                    <h1 className={"text-5xl font-medium"}>50+</h1>
+                                    <h1 className={"my-1"}>project sites</h1>
+                                </List.Section>
+                                <List.Section>
+                                    <h1 className={"text-5xl font-medium"}>8</h1>
+                                    <h1 className={"my-1"}>weeks of community work</h1>
+                                </List.Section>
+                            </List>
                         </h1>
-                        <Stats/>
-                    </div>
-
-                </div>
-            </div>
-            <div className={"border-t border-t-[#e1e1e1] my-24 md:my-36 py-2"} id={"newLine"}>
-                <div className={"grid grid-cols-1 md:grid-cols-2 gap-y-16"}>
-                    <div>
-                        <h1 className={"text-slight-black text-2xl font-medium xl:w-full"}>Context</h1>
-                        <h1 className={"text-newGray text-2xl font-medium xl:w-full max-w-[80%]"} id={"based"}>"How do
-                            we get started?"</h1>
-                    </div>
-                    <div>
+                    </CaseStudySection.Content>
+                </CaseStudySection.SubSection>
+            </CaseStudySection>
+            <CaseStudySection>
+                <CaseStudySection.SubSection>
+                    <CaseStudySection.Title title={"Context"} subtitle={"How do we get started?"}/>
+                    <CaseStudySection.Content>
                         <h1 className={"text-newGray"} id={"based"}>
                             Depending on the project, usually teams must collect vast amounts of different types of data
                             ranging from qualitative,
@@ -51,31 +56,108 @@ export const ProjectBackground = () => {
                             they can focus on the analysis that is
                             important.
                         </h1>
-                        <ProjectGoal/>
-                    </div>
-                </div>
-            </div>
-            <div className={"border-t border-t-[#e1e1e1]  py-2"} id={"newLine"}>
-                <div className={"grid grid-cols-1 md:grid-cols-2 gap-y-16"}>
-                    <div>
-                        <h1 className={"text-slight-black text-4xl font-medium xl:w-full"}>UXR</h1>
-                        <h1 className={"text-newGray text-3xl font-medium xl:w-full mt-1 md:max-w-[80%]"}
-                            id={"based"}>What
-                            do students need?</h1>
-                    </div>
-                    <div>
-                        <Research/>
-                    </div>
-                </div>
-            </div>
-            <div className={"border-t border-t-[#e1e1e1] my-16  py-2"} id={"newLine"}>
-                <div className={"grid grid-cols-1 md:grid-cols-2 gap-y-16"}>
-                    <div>
-                        <h1 className={"text-slight-black text-4xl font-medium xl:w-full"}>User Testing</h1>
-                        <h1 className={"text-newGray text-3xl font-medium xl:w-full md:max-w-[80%] mt-1"}
-                            id={"based"}>What do students think?</h1>
-                    </div>
-                    <div>
+                        <div className={"border-t border-t-[#e1e1e1] mt-8 mb-10 py-2"} id={"newLine"}>
+                            <h1 className={"text-spotify text-sm"} id={"spotifyText"}>Project Goal</h1>
+                            <h1 className={"text-3xl mt-1"}>
+                                Design and build a fullstack app that will methodically standardize data collection
+                                for efficient project workflow.
+                            </h1>
+                        </div>
+                        <List>
+                            <List.Header header={"Key Features"}/>
+                            <List.Section>
+                                <h1>
+                                    IQP teams can create and manage a unique project dashboard for their needs and add
+                                    in team members
+                                    or faculty.
+                                </h1>
+                            </List.Section>
+                            <List.Section>
+                                <h1>
+                                    Every time a member logs data, it will be geotagged where it was taken onto the
+                                    interactive map.
+                                </h1>
+                            </List.Section>
+                            <List.Section>
+                                <h1>
+                                    Many IQP data sites may not have service or wifi. Students can store data in local
+                                    storage until
+                                    syncing at a later time.
+                                </h1>
+                            </List.Section>
+                            <List.Section>
+                                <h1>
+                                    After collecting respective data, a student should be able to export the data onto a
+                                    .csv for further analysis.
+                                </h1>
+                            </List.Section>
+                        </List>
+                    </CaseStudySection.Content>
+                </CaseStudySection.SubSection>
+            </CaseStudySection>
+            <CaseStudySection>
+                <CaseStudySection.SubSection>
+                    <CaseStudySection.Title title={"UXR"} subtitle={"What do students need?"}/>
+                    <CaseStudySection.Content>
+                        <div className={"mt-4"}/>
+                        <List>
+                            <List.Header header={"Three main research pillars"}/>
+                            <List.Section>
+                                <div className={"flex gap-x-1 mb-2"}>
+                                    <ListOrdered className={"text-newGray"} id={"based"}/>
+                                    <h1 className={"text-newGray"} id={"based"}>Prioritization</h1>
+                                </div>
+                                <h1 className={""}>
+                                    We analyzed 20 unique IQP project papers to find common metrics in the way students
+                                    collected data. To minimize error, all team members took part in reviewing another
+                                    peer's
+                                    overview to check
+                                    for any mistakes. We prioritized the highest percentages as our first sprints and
+                                    progressed
+                                    from there. Out
+                                    of those 20, 90% had interviews, 90% qualitative data, 70% surveys, and 50%
+                                    quantitative
+                                    data.
+                                </h1>
+                            </List.Section>
+                            <List.Section>
+                                <div className={"flex gap-x-1 mb-2"}>
+                                    <Sprout className={"text-newGray"} id={"based"}/>
+                                    <h1 className={"text-newGray"} id={"based"}>Viability</h1>
+                                </div>
+                                <h1 className={""}>
+                                    Existing applications such as Fastfield and Jotfield allow form building, geotagging
+                                    data,
+                                    and exportation,
+                                    but what disadvantages to they have? Not free, complicated UI, and is intended for
+                                    the
+                                    electrical industries.
+                                    Our application will be tailored to WPI students as a university service for free
+                                    and aim to
+                                    have a simple UI.
+                                </h1>
+                            </List.Section>
+                            <List.Section>
+                                <div className={"flex gap-x-1 mb-2"}>
+                                    <AppWindow className={"text-newGray"} id={"based"}/>
+                                    <h1 className={"text-newGray"} id={"based"}>Revitalization</h1>
+                                </div>
+                                <h1 className={""}>
+                                    With current WPI pages such as the university site, planner.wpi.edu, and
+                                    courselistings.wpi.edu,
+                                    we will revitalize the current color palette and component structures so users can
+                                    recognize similar
+                                    layouts. Using shadcn UI, our interface brings a new level to the frontier.
+                                </h1>
+                            </List.Section>
+                        </List>
+                    </CaseStudySection.Content>
+                </CaseStudySection.SubSection>
+            </CaseStudySection>
+            <CaseStudySection>
+                <CaseStudySection.SubSection>
+                    <CaseStudySection.Title title={"User Testing"} subtitle={"What do students think?"}/>
+                    <CaseStudySection.Content>
                         <h1 className={"text-newGray"} id={"based"}>
                             Our user testing consisted of 3 weekly sessions, aiming to do 10 tests for the first 2 weeks
                             each, and then finalize
@@ -86,10 +168,29 @@ export const ProjectBackground = () => {
                             such as collapsible accordions for form categories, Form/Link Tile Navigation, and the
                             addition of MCQs.
                         </h1>
-                        <TestingNumbers/>
-                    </div>
-                </div>
-            </div>
+                        <div className={"mt-10"}/>
+                        <List>
+                            <List.Header header={"Students rated 8 or higher on these metrics on a scale of 1-10"}/>
+                            <List.Section>
+                                <h1 className={"text-5xl font-medium"}>88%</h1>
+                                <h1 className={"my-1"}>Overall Satisfaction </h1>
+                            </List.Section>
+                            <List.Section>
+                                <h1 className={"text-5xl font-medium"}>100%</h1>
+                                <h1 className={"my-1"}>Aesthetics</h1>
+                            </List.Section>
+                            <List.Section>
+                                <h1 className={"text-5xl font-medium"}>87%</h1>
+                                <h1 className={"my-1"}>Navigation</h1>
+                            </List.Section>
+                            <List.Section>
+                                <h1 className={"text-5xl font-medium"}>81%</h1>
+                                <h1 className={"my-1"}>Performance</h1>
+                            </List.Section>
+                        </List>
+                    </CaseStudySection.Content>
+                </CaseStudySection.SubSection>
+            </CaseStudySection>
             <div className={"border-t border-t-[#e1e1e1] my-16  py-2"} id={"newLine"}>
                 <h1 className={"text-slight-black text-4xl font-medium xl:w-full"}>Final Design</h1>
                 <Gallery/>
