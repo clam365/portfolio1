a0562155e0c0cb3591efff2923b2664a174a7621
chrislam365 <cllam@wpi.edu>
2026-01-14 00:02:38 -0500

    Kyoto Project Center is done

--- app/kyoto-project-center/page.js
+++ app/kyoto-project-center/page.js
@@ -8,16 +8,33 @@ import CaseStudyStart from "@/components/caseStudy/caseStudyStart";
 export default function Page() {
 
     return (
-        <div>
+        <>
             <CaseStudyStart
                 tag={"FREELANCE"}
                 title={"Kyoto Project Center"}
                 description={"Kyoto IQP Database"}
                 imageSrc={"/mockup1.1.png"}
                 imageAlt={"kyoto"}/>
-            <ProjectIntro/>
-            <ProjectBackground/>
-            <ProjectNext/>
-        </div>
+            <div className={"flex flex-col lg:flex-row"}>
+
+                <nav className="lg:w-1/4 hidden lg:block">
+                    <ul className="space-y-4 sticky top-24 p-10">
+                        <li><a href="#overview" className="hover:text-black text-newGray transition">Overview</a></li>
+                        <li><a href="#challenge" className="hover:text-black text-newGray transition">Challenge</a></li>
+                        <li><a href="#ideation" className="hover:text-black text-newGray transition">Ideation</a></li>
+                        <li><a href="#finaldesigns" className="hover:text-black text-newGray transition">Final Designs</a></li>
+                        <li><a href="#reflections" className="hover:text-black text-newGray transition">Reflections</a></li>
+                    </ul>
+                </nav>
+                <main className="lg:w-2/4 px-10 lg:px-0">
+                    <ProjectIntro/>
+                    <ProjectBackground/>
+                    <ProjectNext/>
+                </main>
+
+
+            </div>
+
+        </>
     );
 }
--- app/kyoto-project-center/project-intro.jsx
+++ app/kyoto-project-center/project-intro.jsx
@@ -1,53 +1,52 @@
-import {ArrowUpRight} from "lucide-react";
 import React from "react";
-import {CaseStudyIntro} from "@/components/caseStudy/caseStudyIntro";
-import {CustomLink} from "@/components/link";
 import {LinkButton} from "@/components/linkButton";
 
 export const ProjectIntro = () => {
     return (
-        <CaseStudyIntro>
-            <CaseStudyIntro.Summary>
-                <h1 className={"text-2xl"}>
-                    In WPI&#39;s archive of IQPs, there are 7,000+ projects listed, and continuously growing. The
-                    <CustomLink link={"https://veniceprojectcenter.org/"} text={" Venice Project Center "}/>
-                    narrows down the search, while providing a distinctive identity and description.
-                    <br/>
-                    <br/>
-                    I led the creation of a project center website for Kyoto,
-                    showcasing 40+ projects as a resource for policymakers, students, and faculty to explore
-                    and gain insights for future work.
-                </h1>
-                <div className={"space-x-3 mt-10"}>
-                    <LinkButton link={"https://kyotoprojectcenter.vercel.app/"} text={"KYOTO PROJECT CENTER"}/>
-                    <LinkButton link={"https://www.wpi.edu/project-based-learning/project-based-education/interactive-qualifying-project"} text={"WPI IQP"}/>
+        <>
+            <div className={"mt-10 "}>
+                {/* BASIC INFO */}
+                <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-6"}>
+                    <div>
+                        <h1 className={"text-newGray"} id={"based"}>ROLE</h1>
+                        <h1>Designer &amp; Developer</h1>
+                    </div>
+                    <div>
+                        <h1 className={"text-newGray"} id={"based"}>TIMELINE</h1>
+                        <h1>Oct 2024 - Dec 2024</h1>
+                    </div>
+                    <div>
+                        <h1 className={"text-newGray"} id={"based"}>TEAM</h1>
+                        <h1>Jason Zhang</h1>
+                        <h1>Arjun Bhat</h1>
+                        <h1>Szymon Mamro</h1>
+                    </div>
+                    <div>
+                        <h1 className={"text-newGray"} id={"based"}>SKILLS</h1>
+                        <h1>Product Design</h1>
+                        <h1>Coding</h1>
+                    </div>
                 </div>
-            </CaseStudyIntro.Summary>
-            <CaseStudyIntro.Info>
-                <CaseStudyIntro.InfoSection>
-                    <h1 className={"text-newGray"} id={"based"}>Product</h1>
-                    <h1>Website Database</h1>
-                </CaseStudyIntro.InfoSection>
-                <CaseStudyIntro.InfoSection>
-                    <h1 className={"text-newGray mb-2"} id={"based"}>Skills</h1>
-                    <h1>Product Design</h1>
-                    <h1>Stakeholder Management</h1>
-                    <h1>Front-End Development</h1>
-                </CaseStudyIntro.InfoSection>
-                <CaseStudyIntro.InfoSection>
-                    <h1 className={"text-newGray mb-2"} id={"based"}>My Role</h1>
-                    <h1>Designer & Developer</h1>
-                </CaseStudyIntro.InfoSection>
-                <CaseStudyIntro.InfoSection>
-                    <h1 className={"text-newGray mb-2"} id={"based"}>Timeline</h1>
-                    <h1>October 18 - December 13, 2024</h1>
-                </CaseStudyIntro.InfoSection>
-                <CaseStudyIntro.InfoSection>
-                    <h1 className={"text-newGray mb-2"} id={"based"}>Team</h1>
-                    <h1>Jason Zhang, Arjun Bhat, Szymon Mamro</h1>
-                </CaseStudyIntro.InfoSection>
-            </CaseStudyIntro.Info>
-        </CaseStudyIntro>
+                {/*Overview*/}
+                <div className={"mt-20 space-y-2 scroll-mt-40"} id={"overview"}>
+                    <h1 className={"text-newGray"}>OVERVIEW</h1>
+                    <h1 className={"font-medium text-3xl"}>Study Abroad in Kyoto, Japan</h1>
+                    <h1>
+                        While working on my team&apos;s humanitarian project of tourism impact in the Kikuhama neighborhood,
+                        one piece of our goal was to create a dedicated platform to showcase 40+ student research projects
+                        for policymakers, students, and business partners.
+                    </h1>
+                    <div className={"inline-flex f gap-3"}>
+                        <LinkButton link={"https://kyotoprojectcenter.vercel.app/"} text={"KYOTO PROJECT CENTER"}/>
+                        <LinkButton
+                            link={"https://www.wpi.edu/project-based-learning/project-based-education/interactive-qualifying-project"}
+                            text={"WPI IQP"}/>
+                    </div>
+                </div>
+            </div>
+
+        </>
+
 
     )
 }
