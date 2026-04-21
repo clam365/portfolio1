665fb53499399b579f969d7486e7ef19af8bca96
chrislam365 <cllam@wpi.edu>
2026-04-20 23:49:57 -0400

    Homepage edits

--- app/design-system/page.js
+++ app/design-system/page.js
@@ -30,6 +30,10 @@ export default function Page() {
                             and welcoming portfolio experience. Built with reusable case study components, tokens, and design standards.
                             I've contributed and built design systems across ALL my experiences, so here's a visualization of mine :)
                         </h1>
+                        <p className={"text-secondary text-sm mt-4"}>
+                            NOTE: Some visualizations were assisted in&nbsp;
+                            <a className={"text-[#de7356] hover:underline transition"} href={"https://claude.com/product/claude-code?utm_source=google&utm_medium=paid_search_coder&utm_campaign=acq_code_us_q3&utm_term=claude%20code&gclsrc=aw.ds&gad_source=1&gad_campaignid=23253558478&gbraid=0AAAAA99jmqvbJ-Tlg9566gY3o5mO_UpG5&gclid=CjwKCAjwnZfPBhAGEiwAzg-VzFWj6_mGWKUVJ4Kn4oKoe9mymB-jeflFStY3_VB0MYhE8Bqaz07kwBoCAdUQAvD_BwE"}>Claude Code</a>
+                        .</p>
                         <section className={"py-10 scroll-mt-24"} id={"colors"}>
                             <h1 className={"text-2xl md:text-2xl font-medium"}>Primitive Colors</h1>
                             <h1 className={"text-md md:text-md text-secondary mt-2"}>These are raw color palette values. Use
